02cac4b97c3d5f91bfaf7b7571f427229ee4c5da
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-22 17:18:20 -0600

    Build out StudentForm class and render elements

--- src/components/StudentForm/StudentForm.js
+++ src/components/StudentForm/StudentForm.js
@@ -5,11 +5,30 @@ import "./StudentForm.css"
 class StudentForm extends Component {
   constructor() {
     super()
+    this.state = {
+      id: 0,
+      name: '',
+      quote: '', 
+      superlative: '', 
+      photo: ''
+
+    }
   }
   render() {
     return(
-    <form>
-
+    <form className="form-container">
+      <label className="form-label">Name:</label>
+      <input className="form-input" type="text" value={this.state.name}/><br/>
+      
+      <label className="form-label">Quote:</label>
+      <input className="form-input" type="text" value={this.state.quote}/><br/>
+      
+      <label className="form-label">Most likely to:</label>
+      <input className="form-input" type="text" value={this.state.superlative}/><br/>
+      
+      <div>
+        <button className="form-button">Submit Student</button>
+      </div>
     </form>
     )
   }
