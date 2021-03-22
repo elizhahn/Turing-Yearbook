0360e4436139c5053efc030906c01afb14414cdd
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-22 08:09:31 -0600

    Add functional component for Person and intital styling

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+function Person({id, photo, name, quote, superlatative}) {
+  return (
+    <article className="card" id={id}>
+      <img src={photo} alt= {"picture of" + name}/>
+      <p>{name}</p>
+      <p>{quote}</p>
+      <p>{superlatative}</p>
+    </article>
+  )
+}
 
 export default Person;
