320a52e3b9a40c90eccf7668b0a76fcb291ed688
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-22 16:25:13 -0600

    Add StudentForm component directory and files

---
+++ src/components/StudentForm/StudentForm.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+import "./StudentForm.css"
+
+
+class StudentForm extends Component {
+  constructor() {
+    super()
+  }
+  render() {
+    return(
+    <form>
+
+    </form>
+    )
+  }
+}
+
+export default StudentForm
