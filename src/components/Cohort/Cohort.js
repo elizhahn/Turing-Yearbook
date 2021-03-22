dba5efdb34e3dc8f7f2e793aa3ae2bf103737b63
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-22 08:23:04 -0600

    Map staff data and pass to Person component

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-function Cohort () {
+function Cohort ({ staff }) {
+  const cohortList = staff.map(person => {
+    return <Person 
+              id={person.id}
+              photo={person.photo}
+              name={person.name}
+              quote={person.quote}
+              superlative={person.superlative}
+           />
+
+  })
   return (
     <main className="card-container">
-      <Person />
+      {cohortList}
     </main>
   )
 }
