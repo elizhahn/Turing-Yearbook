7aa74b507967474dd2f9a2b54c8fb62ce6e26639
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-22 16:27:10 -0600

    Add quotes and spacing for UI

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,9 +4,9 @@ import './Person.css';
 function Person({id, photo, name, quote, superlative}) {
   return (
     <article className="card" id={id}>
-      <img className="card-photo" src={photo} alt= {"picture of" + name}/>
+      <img className="card-photo" src={photo} alt= {"picture of " + name}/>
       <p className="card-name">{name}</p>
-      <p className="card-quote">{quote}</p>
+      <p className="card-quote">"{quote}"</p>
       <p className="card-superlative">{superlative}</p>
     </article>
   )
